65a077c94ea2f1509cba67121c31d7581c50b84e
javiOlaya <jjosaez@gmail.com>
2019-09-27 08:58:16 +0200

    start to create routes

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const bodyParser = require ('body-parser');
+const path = require ('path');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb:localhost/PaginacionBD',
+{useMongoClient:true
+}).then(()=>console.log('DB is connected'))
+.catch(err => console.log(err));
+
+const indexRoutes=require('./routes/index');
+//secciones en el servidor:
+
+//settings
+
+app.set('port', process.env.PORT || 3000);
+app.set('view', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+//middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+//routes
+app.use(indexRoutes);
+//static files
+
+
+app.listen(app.get('port'), ()=>{console.log(' server on ', app.get('port'))
+});
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+const router =require('express').Router();
+
+//routes
+router.get('/',(req, res)=>res.send('PaginationDB!'));
+module.exports =  router;
